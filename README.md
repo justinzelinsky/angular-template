aa3af9f8c986c2b013509e34dde2321e0a9464df
Justin Zelinsky <jpzelinsky@gmail.com>
2016-05-04 22:30:17 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Static Server
+
+A simple [Express](http://expressjs.com) server to host static files
+
+## Setup & Running
+
+### Install node modules with
+
+    npm install
+
+### Run Server
+
+    npm start
---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = process.env.port || 3000;
+
+app.use(express.static('public'));
+
+app.listen(port, function(){
+  console.log('Listening on port %d', port);
+});
