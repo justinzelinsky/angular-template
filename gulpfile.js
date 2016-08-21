39a4b758526e090b05e8ae7578e63be1c619c6e8
Justin Zelinsky <jpzelinsky@gmail.com>
2016-08-21 11:03:11 -0400

    added vendor folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
-var gulp = require('gulp');
-var scss = require('gulp-scss');
-var prefix = require('gulp-autoprefixer');
-var concat = require('gulp-concat');
-var del = require('del');
+const gulp = require('gulp');
+const scss = require('gulp-scss');
+const prefix = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const del = require('del');
 
-var sassFiles = './scss/*.scss';
-var jsFiles = './js/**/*.js';
+const sassFiles = './scss/*.scss';
+const jsFiles = './js/**/*.js';
+
+const vendorCssFiles = './vendor/css/**/*.css';
+const vendorJsFiles = './vendor/js/**/*.js';
+
 
 gulp.task('clean', function() {
   return del(['public/app.js', 'public/style.css']);
@@ -21,7 +25,8 @@ gulp.task('styles', function() {
 });
 
 gulp.task('concat-js', function() {
-  return gulp.src(jsFiles)
+  return gulp
+    .src(jsFiles)
     .pipe(concat('app.js'))
     .pipe(gulp.dest('public'));
 });
